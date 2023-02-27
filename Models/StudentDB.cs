906fd3db0c699de704c7f1575f5b18844c5fcb96
Kirk Herbison <kirk.herbison@gmail.com>
2023-02-27 13:12:25 -0600

    Kirk DB

--- Models/StudentDB.cs
+++ Models/StudentDB.cs
@@ -5,11 +5,16 @@
         static List<Student> listStudent = new List<Student>()
         {
             new Student("Andy", "Bangsberg"),
+<<<<<<< HEAD
+            new Student("Rene", "Bylander")
+=======
             new Student("Rene", "Bylander"),
             new Student("Kyle", "Madlon"),
             new Student("Squirt", "Whispurr"),
             new Student("Ollie", "TheCat"),
+            new Student("Kirk", "Herbison"),
             new Student("Tetyana", "Oldham")
+>>>>>>> origin/master
 
         };
 
