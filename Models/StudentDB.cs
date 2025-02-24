9981baf6ec79eafbfad5b8d1888d301b88c13c1c
G-Skramstad <gabe.skramstad@gmail.com>
2025-02-24 08:53:22 -0600

    added Gabe

--- Models/StudentDB.cs
+++ Models/StudentDB.cs
@@ -21,7 +21,8 @@
             new Student("Jayden", "Visocky"),
             new Student("Alyssa", "Sherman"),
             new Student("Christian", "Eikam"),
-            new Student("Robbie", "Lorenz")
+            new Student("Robbie", "Lorenz"),
+            new Student("Gabe", "Skramstad"),
           };
 
         public static List<Student> GetStudents()
